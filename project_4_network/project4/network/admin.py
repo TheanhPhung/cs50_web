c06edf4d3b650072627ad2ba0d12fa2c9bb6615a
TheanhPhung <jctaf.smode@gmail.com>
2025-02-19 15:57:55 +0700

    Create User, Post, EditedPost, Comment, Like models

--- project_4_network/project4/network/admin.py
+++ project_4_network/project4/network/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
 
-from network.models import User, Post, Like, Comment
-
-
-admin.site.register(User)
-admin.site.register(Post)
-admin.site.register(Like)
-admin.site.register(Comment)
+# Register your models here.
--- project_4_network/project4/network/views.py
+++ project_4_network/project4/network/views.py
@@ -4,41 +4,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from rest_framework import generics
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-from .models import User, Post, Like, Comment
-from .serializers import PostSerializer, UserSerializer, LikeSerializer, CommentSerializer
+from .models import User
 
 
 def index(request):
     return render(request, "network/index.html")
-
-
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class LikeList(generics.ListCreateAPIView):
-    queryset = Like.objects.all()
-    serializer_class = LikeSerializer
-
-
-class LikeDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Like.objects.all()
-    serializer_class = LikeSerializer
 
 
 def login_view(request):
@@ -91,16 +61,3 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
-
-
-@api_view(["GET"])
-def me(request):
-    return Response({"id": request.user.id, "username": request.user.username})
-
-
-@api_view(["GET"])
-def get_like_id(request, user_id, post_id):
-    user = User.objects.get(pk=user_id)
-    post = Post.objects.get(pk=post_id)
-    like = Like.objects.get(liker=user, post=post)
-    return Response({"id": like.id})
